ec643d9dfee1a20f274e49690dee5a7a346ad228
cluisc <luis.j.muc@gmail.com>
2018-04-10 13:35:29 +0200

    updated WebScraper.java

--- app/src/main/java/com/daandtu/webscraper/WebScraper.java
+++ app/src/main/java/com/daandtu/webscraper/WebScraper.java
@@ -1,8 +1,15 @@
 package com.daandtu.webscraper;
 
+import android.content.Context;
+import android.webkit.WebView;
+
 public class WebScraper {
 
-    WebScraper(){
-        
+    private Context context;
+    private WebView web;
+
+    WebScraper(Context context){
+        this.context = context;
+        web = new WebView(context);
     }
 }
