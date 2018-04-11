337f5154cd9c801e849c6064be87e80c929dc3f0
cluisc <luis.j.muc@gmail.com>
2018-04-11 22:41:30 +0200

    updated WebScraper.java

--- app/src/main/java/com/daandtu/webscraper/WebScraper.java
+++ app/src/main/java/com/daandtu/webscraper/WebScraper.java
@@ -1,15 +1,50 @@
 package com.daandtu.webscraper;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.view.View;
 import android.webkit.WebView;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class WebScraper {
 
     private Context context;
     private WebView web;
+    private String URL;
 
     WebScraper(Context context){
         this.context = context;
         web = new WebView(context);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.getSettings().setBlockNetworkImage(true);
+        web.getSettings().setLoadsImagesAutomatically(false);
+    }
+
+    public View getView(){
+        return web;
+    }
+
+    public void setJavaScriptEnabled(boolean enabled){
+        web.getSettings().setJavaScriptEnabled(enabled);
+    }
+
+    public void setLoadImages(boolean enabled){
+        web.getSettings().setBlockNetworkImage(false);
+        web.getSettings().setLoadsImagesAutomatically(true);
+    }
+
+    public void loadURL(String URL){
+        this.URL = URL;
+        web.loadUrl(URL);
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void reload(){
+        web.reload();
     }
 }
+
+
