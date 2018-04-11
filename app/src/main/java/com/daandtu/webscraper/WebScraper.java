8cb56f37d347e4e859884f2eeafed5f050222eda
cluisc <luis.j.muc@gmail.com>
2018-04-11 22:55:11 +0200

    updated WebScraper.java

--- app/src/main/java/com/daandtu/webscraper/WebScraper.java
+++ app/src/main/java/com/daandtu/webscraper/WebScraper.java
@@ -2,8 +2,12 @@ package com.daandtu.webscraper;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class WebScraper {
@@ -15,17 +19,32 @@ public class WebScraper {
     WebScraper(Context context){
         this.context = context;
         web = new WebView(context);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            WebView.enableSlowWholeDocumentDraw();
+        }
         web.getSettings().setJavaScriptEnabled(true);
         web.getSettings().setBlockNetworkImage(true);
         web.getSettings().setLoadsImagesAutomatically(false);
+        JSInterface jInterface = new JSInterface(context);
+        web.addJavascriptInterface(jInterface, "HtmlViewer");
+        web.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+                URL = url;
+                run(view);
+            }
+        });
+    }
+
+    private void run(WebView view){
+
+    }
+
+    public Bitmap takeScreenshot(){
+        return null;
     }
 
     public View getView(){
         return web;
-    }
-
-    public void setJavaScriptEnabled(boolean enabled){
-        web.getSettings().setJavaScriptEnabled(enabled);
     }
 
     public void setLoadImages(boolean enabled){
@@ -45,6 +64,19 @@ public class WebScraper {
     public void reload(){
         web.reload();
     }
+
+
+    private class JSInterface {
+
+        private Context ctx;
+
+        JSInterface(Context ctx) {
+            this.ctx = ctx;
+        }
+
+        @JavascriptInterface
+        public void showHTML(String html) {
+
+        }
+    }
 }
-
-
