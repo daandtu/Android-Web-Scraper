968120392d9c16f91d71849ea01f8d232f664b32
cluisc <luis.j.muc@gmail.com>
2018-04-18 20:50:11 +0200

    updated WebScraper.java

--- app/src/main/java/com/daandtu/webscraper/WebScraper.java
+++ app/src/main/java/com/daandtu/webscraper/WebScraper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Build;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.JavascriptInterface;
@@ -17,9 +18,12 @@ public class WebScraper {
 
     private Context context;
     private WebView web;
+    private String Html;
     private String URL;
 
-    WebScraper(Context context){
+    private onPageLoadedListener onpageloadedlistener;
+
+    WebScraper(final Context context){
         this.context = context;
         web = new WebView(context);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -32,13 +36,17 @@ public class WebScraper {
         web.addJavascriptInterface(jInterface, "HtmlViewer");
         web.setWebViewClient(new WebViewClient() {
             public void onPageFinished(WebView view, String url) {
-                URL = url;
-                run(view);
+
+                if (onpageloadedlistener!=null){
+                    onpageloadedlistener.loaded(url);
+                }
+                view.loadUrl("javascript:window.HtmlViewer.showHTML" +
+                        "(document.getElementsByTagName('html')[0].innerHTML);");
             }
         });
     }
 
-    private void run(WebView view){
+    public void run(){
 
     }
 
@@ -53,8 +61,16 @@ public class WebScraper {
         return bitmap;
     }
 
+    public Bitmap takeScreenshot(int maxWidthOrHeight){
+        return resizeBitmap(takeScreenshot(),maxWidthOrHeight);
+    }
+
     public View getView(){
         return web;
+    }
+
+    public String getHtml(){
+        return Html;
     }
 
     public void setLoadImages(boolean enabled){
@@ -86,7 +102,36 @@ public class WebScraper {
 
         @JavascriptInterface
         public void showHTML(String html) {
-
+            Html = html;
         }
+    }
+
+
+    private Bitmap resizeBitmap(Bitmap bitmap, int scaleSize) {
+        Bitmap resizedBitmap;
+        int originalWidth = bitmap.getWidth(), originalHeight = bitmap.getHeight(), newWidth = -1, newHeight = -1;
+        float fac;
+        if(originalHeight > originalWidth) {
+            newHeight = scaleSize ;
+            fac = (float) originalWidth/(float) originalHeight;
+            newWidth = (int) (newHeight*fac);
+        } else if(originalWidth > originalHeight) {
+            newWidth = scaleSize ;
+            fac = (float) originalHeight/ (float)originalWidth;
+            newHeight = (int) (newWidth*fac);
+        } else if(originalHeight == originalWidth) {
+            newHeight = scaleSize ;
+            newWidth = scaleSize ;
+        }
+        resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, false);
+        return resizedBitmap;
+    }
+
+    public void setOnPageLoadedListener(onPageLoadedListener onpageloadedlistener){
+        this.onpageloadedlistener = onpageloadedlistener;
+    }
+
+    public interface onPageLoadedListener{
+        void loaded(String URL);
     }
 }
