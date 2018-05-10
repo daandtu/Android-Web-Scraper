cf1eccfc797bf1eeefcfdad6106bc874aed606e5
cluisc <luis.j.muc@gmail.com>
2018-05-10 22:52:05 +0200

    updated WebScraper.java added Element.java

--- app/src/main/java/com/daandtu/webscraper/Element.java
+++ app/src/main/java/com/daandtu/webscraper/Element.java
@@ -1,20 +1,22 @@
 package com.daandtu.webscraper;
 
-public class Element {
+class Element {
 
-    Element (WebScraper web){
+    private String elementLocator;
+    private WebScraper web;
+
+    Element (WebScraper web, String elementLocator){
+        this.web = web;
+        this.elementLocator = elementLocator;
 
     }
 
     void setText(String text){
-
+        web.run("javascript:" + elementLocator + ".value = '" + text + "';void(0);");
     }
 
     void click(){
-
+        web.run("javascript:" + elementLocator + ".click();void(0);");
     }
 
-    String getText(){
-        return "";
-    }
 }
