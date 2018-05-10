62a60d567399433a9090a565b9ee9406ebf897d6
cluisc <luis.j.muc@gmail.com>
2018-05-10 22:27:08 +0200

    updated WebScraper.java

---
+++ app/src/main/java/com/daandtu/webscraper/Element.java
@@ -0,0 +1,20 @@
+package com.daandtu.webscraper;
+
+public class Element {
+
+    Element (WebScraper web){
+
+    }
+
+    void setText(String text){
+
+    }
+
+    void click(){
+
+    }
+
+    String getText(){
+        return "";
+    }
+}
