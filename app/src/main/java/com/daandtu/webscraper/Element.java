861a8039cd08bf5a2d5a8e53d0851840b40ed734
cluisc <luis.j.muc@gmail.com>
2018-05-21 00:51:55 +0200

    updated WebScraper.java updated Element.java

--- app/src/main/java/com/daandtu/webscraper/Element.java
+++ app/src/main/java/com/daandtu/webscraper/Element.java
@@ -7,28 +7,32 @@ class Element {
     private String elementLocator;
     private WebScraper web;
 
-    Element (WebScraper web, String elementLocator){
+    public Element (WebScraper web, String elementLocator){
         this.web = web;
         this.elementLocator = elementLocator;
 
     }
 
-    void setText(String text){
+    public void setText(String text){
         String t = "javascript:" + elementLocator + ".value='" + text + "';void(0);";
         Log.i("Logmsg",t);
         web.run(t);
     }
 
-    void click(){
+    public void click(){
         web.run("javascript:" + elementLocator + ".click();void(0);");
     }
 
-    void getText(){
-        web.run2("javascript:window.HtmlViewer.processContent(" + elementLocator + ".innerText);");
+    public String getText(){
+        return web.run2("javascript:window.HtmlViewer.processContent(" + elementLocator + ".innerText);");
     }
 
-    void getValue(){
-        web.run2("javascript:window.HtmlViewer.processContent(" + elementLocator + ".value);");
+    public String getValue(){
+        return web.run2("javascript:window.HtmlViewer.processContent(" + elementLocator + ".value);");
+    }
+
+    public String getName(){
+        return web.run2("javascript:window.HtmlViewer.processContent(" + elementLocator + ".name);");
     }
 
 }
